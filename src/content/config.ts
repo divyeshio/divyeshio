68aae670c7e8cc15f95995414bbae291a4942dce
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2023-12-08 14:46:23 +0530

    Add portfolio template

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+export const collections = {
+	work: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
 	}),
-});
-
-export const collections = { blog };
+};
